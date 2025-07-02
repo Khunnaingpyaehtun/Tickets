2fc37e1b9b6bf3f4ba5d7eeb60e0b277a2d8a4b5
Khunnaingpyaehtun <khunnaingpyaehtun@gmail.com>
2025-07-02 13:11:08 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // ဒီနေရာမှာ သင့်ရဲ့ html နဲ့ js file တွေကို ရှာခိုင်းတာပါ
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
